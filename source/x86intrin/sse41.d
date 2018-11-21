3355039e78e716b94c7d3f6fb96bf538d81a4d80
Hiroki Noda <kubo39@gmail.com>
2018-11-22 08:06:37 +0900

    Add test for testz_si128

--- source/x86intrin/sse41.d
+++ source/x86intrin/sse41.d
@@ -66,3 +66,19 @@ int _mm_testnzc_si128(long2, long2) pure @safe;
 
 pragma(LDC_intrinsic, "llvm.x86.sse41.ptestz")
 int _mm_testz_si128(long2, long2) pure @safe;
+
+unittest
+{
+
+    // testz
+    {
+        long2 x = [0, 0, 0, 0];
+        long2 y = [1, 1, 1, 1];
+        long2 z = [2, 2, 2, 2];
+
+        assert(_mm_testz_si128(x, x) == 1);
+        assert(_mm_testz_si128(y, y) == 0);
+        assert(_mm_testz_si128(z, z) == 0);
+        assert(_mm_testz_si128(y, z) == 1);
+    }
+}
