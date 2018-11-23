092fa48014ffc29067f7ddedf4bb2d331739388c
Hiroki Noda <kubo39@gmail.com>
2018-11-23 11:23:30 +0900

    Fix cmpestrX intrinsics

--- source/x86intrin/sse42.d
+++ source/x86intrin/sse42.d
@@ -11,26 +11,48 @@ import core.simd;
 import ldc.intrinsics;
 import ldc.simd;
 
+immutable int SIDD_UBYTE_OPS = 0x00;
+immutable int SIDD_UWORD_OPS = 0x01;
+immutable int SIDD_SBYTE_OPS = 0x02;
+immutable int SIDD_SWORD_OPS = 0x03;
+
+immutable int SIDD_CMP_EQUAL_ANY = 0x00;
+immutable int SIDD_CMP_RANGES = 0x04;
+immutable int SIDD_CMP_EQUAL_EACH = 0x08;
+immutable int SIDD_CMP_EQUAL_ORDERED = 0x0c;
+
+immutable int SIDD_POSITIVE_POLARITY = 0x00;
+immutable int SIDD_NEGATIVE_POLARITY = 0x10;
+immutable int SIDD_MASKED_POSITIVE_POLARITY = 0x20;
+immutable int SIDD_MASKED_NEGATIVE_POLARITY = 0x30;
+
+immutable int SIDD_LEAST_SIGNIFICANT = 0x00;
+immutable int SIDD_MOST_SIGNIFICANT = 0x40;
+
+immutable byte SIDD_BIT_MASK = 0x00;
+immutable byte SIDD_UNIT_MASK = 0x40;
+
+
 pragma(LDC_intrinsic, "llvm.x86.sse42.pcmpestrm128")
-byte16 _mm_cmpestrm(byte16, byte16, int, ubyte) pure @safe;
+byte16 _mm_cmpestrm(byte16, int, byte16, int, ubyte) pure @safe;
 
 pragma(LDC_intrinsic, "llvm.x86.sse42.pcmpestri128")
-int _mm_cmpestri(byte16, byte16, int, ubyte) pure @safe;
+int _mm_cmpestri(byte16, int, byte16, int, ubyte) pure @safe;
 
 pragma(LDC_intrinsic, "llvm.x86.sse42.pcmpestria128")
-int _mm_cmpestra(byte16, byte16, int, ubyte) pure @safe;
+int _mm_cmpestra(byte16, int, byte16, int, ubyte) pure @safe;
 
 pragma(LDC_intrinsic, "llvm.x86.sse42.pcmpestric128")
-int _mm_cmpestrc(byte16, byte16, int, ubyte) pure @safe;
+int _mm_cmpestrc(byte16, int, byte16, int, ubyte) pure @safe;
 
 pragma(LDC_intrinsic, "llvm.x86.sse42.pcmpestrio128")
-int _mm_cmpestro(byte16, byte16, int, ubyte) pure @safe;
+int _mm_cmpestro(byte16, int, byte16, int, ubyte) pure @safe;
 
 pragma(LDC_intrinsic, "llvm.x86.sse42.pcmpestris128")
-int _mm_cmpestrs(byte16, byte16, int, ubyte) pure @safe;
+int _mm_cmpestrs(byte16, int, byte16, int, ubyte) pure @safe;
 
 pragma(LDC_intrinsic, "llvm.x86.sse42.pcmpestriz128")
-int _mm_cmpestrz(byte16, byte16, int, ubyte) pure @safe;
+int _mm_cmpestrz(byte16, int, byte16, int, ubyte) pure @safe;
 
 
 pragma(LDC_intrinsic, "llvm.x86.sse42.pcmpistrm128")
