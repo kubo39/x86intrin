129ca5180dc7e4666062bb3caa8871f7ffad2262
Hiroki Noda <kubo39@gmail.com>
2018-11-24 06:45:11 +0900

    Add tests

--- tests/sse41.d
+++ tests/sse41.d
@@ -1,6 +1,30 @@
 import x86intrin;
 
-void test_testz_si128()
+void test_mm_testc_si128()
+{
+    int4 x = [0x7, 0x7, 0x7, 0x7];
+    int4 y = [0x3, 0x3, 0x3, 0x3];
+    int4 z = [0x8, 0x8, 0x8, 0x8];
+
+    assert(_mm_testc_si128(x, y) == 1);
+    assert(_mm_testc_si128(x, z) == 0);
+}
+
+void test_mm_testnzc_si128()
+{
+    {
+        int4 a = [0, 0, 0, 0];
+        int4 b = [~0, ~0, 0, 0];
+        assert(_mm_testnzc_si128(a, b) == 0);
+    }
+    {
+        int4 a = [1, 0, 0, 0];
+        int4 b = [~0, ~0, 0, 0];
+        assert(_mm_testnzc_si128(a, b) == 1);
+    }
+}
+
+void test_mm_testz_si128()
 {
     long2 x = [0, 0];
     long2 y = [(1L << 32) + 1, (1L << 32) + 1];
@@ -14,5 +38,7 @@ void test_testz_si128()
 
 void main()
 {
-    test_testz_si128();
+    test_mm_testc_si128();
+    test_mm_testnzc_si128();
+    test_mm_testz_si128();
 }
