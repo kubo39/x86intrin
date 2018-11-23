81f63ae5950ea92fc4c99efedbe99ae4ffc1ee80
Hiroki Noda <kubo39@gmail.com>
2018-11-24 08:05:29 +0900

    Remove unnecessary cast

--- tests/sse3.d
+++ tests/sse3.d
@@ -4,8 +4,8 @@ void test_lddqu()
 {
     int4 x = [1, 2, 3, 4];
     auto p = x.ptr;
-    auto r = _mm_lddqu_si128(cast(const ubyte*)p);
-    assert(cast(int4) r == [1, 2, 3, 4]);
+    int4 r = _mm_lddqu_si128(cast(const ubyte*)p);
+    assert(r == [1, 2, 3, 4]);
 }
 
 void main()
