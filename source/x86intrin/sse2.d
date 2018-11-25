5d8305b3ea90dd475f17d3ee3ec3e5b11436095e
Hiroki Noda <kubo39@gmail.com>
2018-11-26 01:21:45 +0900

    Comment some functions

--- source/x86intrin/sse2.d
+++ source/x86intrin/sse2.d
@@ -95,27 +95,27 @@ pragma(LDC_intrinsic, "llvm.x86.sse2.pause")
 void _mm_pause() pure @safe;
 
 
-byte16 _mm_set1_epi8(byte a) pure
-{
-    byte[16] result = [a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a];
-    return loadUnaligned!(byte16)(result.ptr);
-}
+// byte16 _mm_set1_epi8(byte a) pure
+// {
+//     byte[16] result = [a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a];
+//     return loadUnaligned!(byte16)(result.ptr);
+// }
 
-byte16 _mm_setr_epi8(byte e15, byte e14, byte e13, byte e12,
-                     byte e11, byte e10, byte e9, byte e8,
-                     byte e7, byte e6, byte e5, byte e4,
-                     byte e3, byte e2, byte e1, byte e0) pure
-{
-    byte[16] result = [e15, e14, e13, e12, e11, e10, e9, e8,
-                       e7, e6, e5, e4, e3, e2, e1, e0];
-    return loadUnaligned!(byte16)(result.ptr);
-}
+// byte16 _mm_setr_epi8(byte e15, byte e14, byte e13, byte e12,
+//                      byte e11, byte e10, byte e9, byte e8,
+//                      byte e7, byte e6, byte e5, byte e4,
+//                      byte e3, byte e2, byte e1, byte e0) pure
+// {
+//     byte[16] result = [e15, e14, e13, e12, e11, e10, e9, e8,
+//                        e7, e6, e5, e4, e3, e2, e1, e0];
+//     return loadUnaligned!(byte16)(result.ptr);
+// }
 
-int4 _mm_setr_epi32(int i0, int i1, int i2, int i3) pure
-{
-    int[4] result = [i0, i1, i2, i3];
-    return loadUnaligned!(int4)(result.ptr);
-}
+// int4 _mm_setr_epi32(int i0, int i1, int i2, int i3) pure
+// {
+//     int[4] result = [i0, i1, i2, i3];
+//     return loadUnaligned!(int4)(result.ptr);
+// }
 
 byte16 _mm_add_epi8(byte16 a, byte16 b) pure
 {
